d350caafb4ce3d7c41518914b899fb80c250fc96
Kieran <kieran.webdevelopment@gmail.com>
2022-06-10 12:17:12 +0100

    rough to-do list completed

--- hackathon-todo-list/src/components/App/App.js
+++ hackathon-todo-list/src/components/App/App.js
@@ -2,22 +2,59 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [newItem, setNewItem] = useState('');
+  const [items, setItems] = useState([]);
 
-  const [newItem, setNewItem] = useState("");
+  function addToList(e) {
+    setNewItem(e.target.value);
+    // console.log(newItem);
+  }
+
+  function addItem() {
+    // console.log(newItem);
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem,
+    };
+
+    setItems((oldList) => [...oldList, item]);
+    setNewItem('');
+    // console.log(items);
+
+    if (!newItem) {
+      alert('Please enter something');
+    }
+    return;
+  }
+
+  function deleteItem(id) {
+    const newArray = items.filter((item) => item.id !== id);
+    setItems(newArray);
+  }
 
   return (
     <div className="App">
-    <h1>To-do List</h1>
-    <input
-    type="text"
-    placeholder='Add item...'
-    />
-    <button>Add to list</button>
-    <ul>
-    <li>Learn React</li>
-    <li>Break the problem down!</li>
-    <li>Do some code wars</li>
-    </ul>
+      <h1>To-do List</h1>
+      <input
+        type="text"
+        placeholder="Add item..."
+        value={newItem}
+        onChange={addToList}
+      />
+
+      <button onClick={() => addItem()}>Add to list</button>
+
+      <ul>
+        {items.map((item) => {
+          return (
+            <li key={item.id}>
+              {item.value}{' '}
+              <button onClick={() => deleteItem(item.id)}>Done!</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
